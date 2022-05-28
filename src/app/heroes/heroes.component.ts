8f4b64370ce33c09b06c392e9dbd0a75cd328ac7
Enmajs <enmajs>
2022-05-28 14:43:08 -0500

    create list with data to iterate into any components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero} from '../../core/Interfaces/Hero'
-
+import {HEROES} from '../../core/Constants/Heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
+  heroes = HEROES;
   title = 'Heroes component';
+  selectedHero?: Hero;
   hero: Hero = {
     id: 0,
     name: 'name Hero Component',
@@ -19,5 +20,8 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/core/Constants/Heroes.ts
@@ -0,0 +1,16 @@
+import { Hero} from '../Interfaces/Hero'
+
+export const HEROES: Hero[] = [
+    {
+        id: 1,
+        name: 'superman',
+        phone: 1234,
+        status: true
+    },
+    {
+        id: 2,
+        name: 'batman',
+        phone: 1235,
+        status: true
+    }
+]
