4bbebed5301dd3c3cea14e0f6c5c763515fb1101
Enmajs <enmajs>
2022-05-28 19:28:58 -0500

    observables

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero} from '../../core/Interfaces/Hero'
-import {HEROES} from '../../core/Constants/Heroes'
+import { Hero } from '../../core/Interfaces/Hero'
+import { HEROES } from '../../core/Constants/Heroes'
+import { HeroService } from '../hero.service'
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   title = 'Heroes component';
   selectedHero?: Hero;
   hero: Hero = {
@@ -16,12 +18,19 @@ export class HeroesComponent implements OnInit {
     phone: 0,
     status: false
   }
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+    console.log('data', this.heroes)
   }
 }
